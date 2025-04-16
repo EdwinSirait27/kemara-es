821dbf67bc4a4c0f833e8864e8ce33b7b682b5f0
eds nigel <drummerboy794@gmail.com>
2025-04-16 11:53:33 +0800

    first commit

--- resources/views/session/login-session.blade.php
+++ resources/views/session/login-session.blade.php
@@ -51,7 +51,7 @@
 <section class="min-vh-100 mb-8">
     <div class="page-header align-items-start min-vh-75 pt-5 pb-11 mx-3 border-radius-lg"
     {{-- DISINI --}}
-        style="background-image: url('{{ asset('assets/img/white-curved.jpeg') }}');">
+        style="background-image: url('{{ asset('assets/img/Picture1.png') }}');">
         {{-- style="background-image: url('{{ asset('assets/img/curved-images/white-curved.jpeg') }}');"> --}}
         <span class="mask bg-gradient-dark opacity-6"></span>
         <div class="container">
